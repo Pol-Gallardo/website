108b3cd38112b0883095f69bb6c411f8aee9f090
polgallardo <pol.gallardo96@gmail.com>
2023-11-24 19:50:10 +0100

    updates

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
                     { /* mail */ }
                     <div className="flex items-center gap-x-8">
                         <MailIcon size={18} className="text-primary" />
-                        <div>info@polgallardo.com</div>
+                        <div>pol.gallardo96@gmail.com</div>
                     </div>
                     { /* address */ }
                     <div className="flex items-center gap-x-8">
